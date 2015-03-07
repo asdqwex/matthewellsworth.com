4e130af5faa8ce5dcc331061c91f4777e8106b36
unknown <matt.ellsworth@asdqwex.net>
2015-03-07 01:34:30 -0600

    update.php

---
+++ dist/update.php
@@ -0,0 +1,22 @@
+<?PHP
+
+#error_reporting(0);
+
+require 'secretfile.php';
+
+list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE']);
+$payload = file_get_contents('php://input');
+$data = json_decode($payload);
+
+$payloadHash = hash_hmac($algo, $payload, $secret);
+
+if ($hash !== $payloadHash) {
+  header("HTTP/1.0 404 Not Found");
+  exit;
+}
+
+exec("sudo salt-call --local state.highstate 2>&1", $output);
+
+print_r($output);
+
+?>
